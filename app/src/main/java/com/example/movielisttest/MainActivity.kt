aadeb71f9f3dba41f25fd3f52a60ddb34f63e146
Oscar Gustavo Moreno Bautista <oscar.moreno@enhanceit.us>
2022-10-14 13:22:14 -0400

    Introduction to CardView
    Adding CardView in layout folder. We can use tools text instead of common property just to double check a text preview

--- app/src/main/java/com/example/movielisttest/MainActivity.kt
+++ app/src/main/java/com/example/movielisttest/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.movielisttest
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
+

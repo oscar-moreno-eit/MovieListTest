4e71f9968946860682867c2d48c745da7b7d06e2
Oscar Gustavo Moreno Bautista <oscar.moreno@enhanceit.us>
2022-10-20 11:58:30 -0400

    Implemented movie api consumption

---
+++ app/src/main/java/com/example/movielisttest/model/remote/MovieNetwork.kt
@@ -0,0 +1,89 @@
+package com.example.movielisttest.model.remote
+
+import android.net.Uri
+import com.example.movielisttest.model.MovieResponse
+import com.example.movielisttest.model.Response
+import org.json.JSONArray
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+
+class MovieNetwork {
+    //
+    companion object{
+        const val BASE_URL = "https://gist.githubusercontent.com/"
+        const val ENDPOINT = "AntoninoAN/f3fa4b2260c51a5f80904c747009289e/raw/6576691177f6b093afd3bf2bbc5e936b62d50721/MovieGist"
+        val uriPath = Uri.parse("$BASE_URL$ENDPOINT")
+        val url = URL(uriPath.toString())
+    }
+
+    fun getMovieList(): Response{
+        val httpUrlConnection = url.openConnection() as HttpURLConnection
+
+        httpUrlConnection.readTimeout = 10000
+        httpUrlConnection.connectTimeout = 15000
+        httpUrlConnection.requestMethod = "GET"
+        httpUrlConnection.doInput = true
+
+        httpUrlConnection.connect()
+
+        //httpUrlConnection.responseCode
+        //httpUrlConnection.inputStream
+
+        //val response: Response =
+            httpUrlConnection.inputStream.run {
+            deserializeIS(this)
+        }.let {
+            return  parseToMovieResponse(it)
+        }
+
+    }
+
+    fun  deserializeIS(iS: InputStream): String{
+        val builder = StringBuilder()
+        val reader = BufferedReader(InputStreamReader(iS))
+        var line: String?  = reader.readLine()
+
+        while (line != null){
+            builder.append(line)
+            builder.append("\n")
+            line = reader.readLine()
+
+        }
+
+
+
+        if (builder.isEmpty()) return "N/A"
+        return builder.toString()
+    }
+
+    fun parseToMovieResponse(deserialize: String): Response{
+        val response = JSONArray(deserialize)
+        val movieResponseList = Response()
+        val listOfMovies = mutableListOf<MovieResponse>()
+        for(index in 0 until response.length()){// index =  [0,1,2,...response.length}
+            val movieElement = response.getJSONObject(index)
+            val movieResponse = MovieResponse(rating = movieElement.getDouble("rating").toFloat()
+            , image = movieElement.getString("image")
+            , title = movieElement.getString("title")
+            , releaseYear = movieElement.getInt("releaseYear")
+            , genre = movieElement.getJSONArray("genre").parseJsonArrayToList()
+            )
+
+            movieResponseList.add(movieResponse)
+            listOfMovies.add(movieResponse)
+        }
+        return movieResponseList
+    }
+
+    private fun JSONArray.parseJsonArrayToList(): List<String>{// Extension function
+        val result = mutableListOf<String>()
+        for  (i in 0 until  this.length()){
+            result.add(this.getString(i))
+        }
+        return result
+    }
+
+}

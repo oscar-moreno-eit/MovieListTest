daaae0bfd3c35c62514bb942b1ba3f1892f269fd
Oscar Gustavo Moreno Bautista <oscar.moreno@enhanceit.us>
2022-10-22 22:50:18 -0400

    Adding adapter to recycler view

--- app/src/main/java/com/example/movielisttest/MainActivity.kt
+++ app/src/main/java/com/example/movielisttest/MainActivity.kt
@@ -2,26 +2,66 @@ package com.example.movielisttest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.movielisttest.model.Response
+import com.example.movielisttest.model.remote.MovieNetwork
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var rvMovieList:  RecyclerView
+
+    private val movieHandler =
+        object: Handler(){
+            override fun handleMessage(msg: Message) {
+                super.handleMessage(msg)
+
+                when(msg.what){
+                    1-> {
+                        val listOfMovies: Response = msg.obj as Response
+                        Log.d(TAG, "handleMessage: $listOfMovies")
+                        var list = mutableListOf<String>()
+                        for(index in 0 until listOfMovies.size){
+                            list.add(index,listOfMovies[index].title)
+                        }
+                        rvMovieList.adapter = MovieAdapter(list)
+
+                    }
+                    else -> {
+                        msg.data?.getString("KEY")?.let {
+                            Toast.makeText(this@MainActivity,
+                                it, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+
+
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initViews()
+
+        getMovieList()
     }
 
     private fun initViews() {
 
         rvMovieList  = findViewById(R.id.movie_list)
-        rvMovieList.adapter = MovieAdapter(generateData())
+        //rvMovieList.adapter = MovieAdapter(getMovieList())
         rvMovieList.layoutManager = createLayoutManager()
 
     }
@@ -33,10 +73,29 @@ class MainActivity : AppCompatActivity() {
         return linearLayoutManager
     }
 
-    private fun generateData(): List<String> {
-        return(0..99).map {
-            "Movie with the name: $it"
-        }
+    private fun getMovieList(){
+        val network = MovieNetwork()
+        var networkResult = mutableListOf<String>()
+
+        Thread{
+            Log.d(TAG, "getMovieList: ${Thread.currentThread().name}")
+            val message = Message()
+            message.what = 1
+            message.obj = network.getMovieList()
+            movieHandler.sendMessage(message)
+        }.start()
+
+        Thread(Runnable {
+            movieHandler.sendMessage(
+                Message().apply {
+                    what = 2
+                    data = Bundle().apply {
+                        putString("KEY","${Thread.currentThread().name}")
+                    }
+                }
+            )
+        }).start()
+
     }
 }
 
